c208ec14afdb898492393ccd3fcecd85166f7808
Diam Diallo <diamdiallo98@gmail.com>
2023-08-11 21:16:06 +0000

    first commit

---
+++ src/main/java/com/example/poaccessingdata/PoAccessingDataApplication.java
@@ -0,0 +1,13 @@
+package com.example.poaccessingdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoAccessingDataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PoAccessingDataApplication.class, args);
+    }
+
+}
